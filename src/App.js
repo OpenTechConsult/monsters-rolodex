cff1a855ff0a6cb714ad3112f6a4f58f84d23e5a
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-28 19:18:28 +0000

    Project: Monster-Rolodex
    Complete the monster rolodex application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { CardList } from './components/card-list/card-list.component'
-import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
+import './App.css'
 
 class App extends Component {
 
     constructor() {
         super ();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
 
     }
@@ -18,12 +20,20 @@ class App extends Component {
         .then(users => this.setState({ monsters: users}));
     }
 
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value })
+    }
+
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster => 
+                monster.name.toLowerCase().includes(searchField.toLowerCase()))
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters}>
-                  
-                </CardList>
+                <h1>Monsters Rolodex</h1>
+                <SearchBox placeholder='search monster' 
+                            handleChange={ this.handleChange } />
+                <CardList monsters={filteredMonsters} />
                 
             </div>
         );
